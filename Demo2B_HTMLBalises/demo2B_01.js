82a689e9e81209c18b0a456d87532a99e85d04d0
wlost68 <51869591+wlost68@users.noreply.github.com>
2020-02-03 19:28:17 +0200

    add demo

---
+++ Demo2B_HTMLBalises/demo2B_01.js
@@ -0,0 +1,7 @@
+// Fichier utilisé dans les démos et exercice du chapitre 2B #01
+// Auteur    : Pierre Coutu
+// Date      : 29/01/2020
+// Important : Nous allons voir ces concepts en détails au chapitre 6.
+function afficherCoucou(){
+  alert("Coucou");	
+}
